dc073a18a19db6f1825685fefd19da388d5f9df5
Zeyad Nofal <zeyadnofal98@gmail.com>
2021-04-19 02:13:47 +0200

    First Lab
    Create simple database using entity framework core

---
+++ Lab1-UniversityApp/Lab1-UniversityApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UniversityApp.Data;
+using UniversityApp.Domain;
+
+namespace UniversityApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new UniversityDbContext();
+            db.Universities.Add(new University() { Name = "Ahram Canadian University" });
+            db.Students.Add(new Student() { Name = "Zeyad", Grade = 76 });
+            db.SaveChanges();
+            Console.WriteLine("Database saved successfully.");
+        }
+    }
+}
---
+++ Lab1-UniversityApp/UniversityApp.Data/UniversityDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UniversityApp.Domain;
+
+namespace UniversityApp.Data
+{
+    public class UniversityDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<University> Universities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=University;Integrated Security=True");
+        }
+    }
+}
